123a7ac155274abc809a8160cff4fe4527753f9b
datstar2 <pssd12@gmail.com>
2020-09-10 20:25:56 +0900

    exception handling

--- board/views.py
+++ board/views.py
@@ -2,8 +2,6 @@ from django.http import HttpResponse, HttpResponseRedirect
 import board.models as boardmodel
 from django.shortcuts import render
 
-
-
 def index(request):
     totalcount = boardmodel.fetchlistcount()
     countpage = 10
@@ -41,10 +39,14 @@ def modifyform(request):
     no = request.GET['no']
     results = boardmodel.fetchonebyno(no)
     data = {'board': results}
-    if data['board']['user_no'] != request.session['auth']['no']:
+    try:
+        if data['board']['user_no'] == request.session['auth']['no']:
+            return render(request, 'board/modifyform.html', data)
+        else:
+            return HttpResponseRedirect('/board?page=1')
+    except KeyError as e:
         return HttpResponseRedirect('/board?page=1')
-    else:
-        return render(request, 'board/modifyform.html', data)
+
 
 def modify(request):
     no = request.GET['no']
@@ -60,17 +62,20 @@ def replyform(request):
     return render(request, 'board/replyform.html', data)
 
 def reply(request):
-    g_no = request.POST['g_no']
-    o_no = str(int(request.POST['o_no'])+1)
-    depth = str(int(request.POST['depth'])+1)
-    title = request.POST['title']
-    content = request.POST['content']
-    user_no = request.session['auth']['no']
-    boardmodel.insert(title, content, user_no)
-    no = boardmodel.maxno()
-    boardmodel.updatereply(g_no, o_no, depth, no)
-    boardmodel.neworder(g_no, o_no, no)
-    return HttpResponseRedirect('/board?page=1')
+    try:
+        g_no = request.POST['g_no']
+        o_no = str(int(request.POST['o_no'])+1)
+        depth = str(int(request.POST['depth'])+1)
+        title = request.POST['title']
+        content = request.POST['content']
+        user_no = request.session['auth']['no']
+        boardmodel.insert(title, content, user_no)
+        no = boardmodel.maxno()
+        boardmodel.updatereply(g_no, o_no, depth, no)
+        boardmodel.neworder(g_no, o_no, no)
+        return HttpResponseRedirect('/board?page=1')
+    except KeyError as e:
+        return HttpResponseRedirect('/board?page=1')
 
 def delete(request):
     no = request.GET['no']
